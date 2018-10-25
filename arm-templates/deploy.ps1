545eeaa40bfd3b4dd9ad3c580f9e3bbed48aa090
Santiago Marrone <santiagocmarrone@gmail.com>
2018-10-25 00:31:29 -0300

    ARM Templates Deployment

---
+++ arm-templates/deploy.ps1
@@ -0,0 +1,22 @@
+﻿function Test-Deployment {
+        
+    New-AzureRmResourceGroup -Name test-development -Location eastus
+
+    New-AzureRmResourceGroupDeployment `
+    -ResourceGroupName test-development `
+    -TemplateFile C:\Users\santiago.marrone\Documents\academy-remote\template-deployment\template.json `
+    -TemplateParameterFile C:\Users\santiago.marrone\Documents\academy-remote\template-deployment\parameters-development.json
+    
+    Remove-AzureRmResourceGroup -Name test-development
+}
+
+Connect-AzureRmAccount
+
+$continue = "yes"
+
+while ($continue -eq "yes") { 
+        
+    Test-Deployment
+
+    $continue = Read-Host "Continue?"
+}
